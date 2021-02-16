23b41a4301503f16df9450ab2ddf3439cde6a793
kromate <akpananthony33@gmail.com>
2021-02-16 15:49:07 +0100

    started ui

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,18 +1,32 @@
 <template>
-  <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png" />
-    <HelloWorld msg="Welcome to Your Vue.js App" />
+  <div class="is-flex is-flex-direction-column is-justify-content-center is-align-content-center">
+    <div class="is-flex w-50">
+      <input class="input is-link" type="text" placeholder="Link input" />
+      <button class="button is-info">Info</button>
+    </div>
+
+    <div class="card">
+      <header class="card-header">
+        <p class="card-header-title">
+          Card header
+        </p>
+        <button class="card-header-icon" aria-label="more options">
+          <span class="icon">
+            <i class="fas fa-angle-down" aria-hidden="true"></i>
+          </span>
+        </button>
+      </header>
+    </div>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
-import HelloWorld from "@/components/HelloWorld.vue";
-
 export default {
   name: "Home",
-  components: {
-    HelloWorld
-  }
 };
 </script>
+
+<style scoped>
+.w-50 {
+}
+</style>
