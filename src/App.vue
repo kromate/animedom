188d4cc00eb5769e47e75ce2d634554a4ad0f90d
kromate <akpananthony33@gmail.com>
2021-02-17 21:23:18 +0100

    update: adding basic css

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,30 @@
 <style>
 body {
   background: rgb(2, 36, 10);
-  min-width: 100vw;
-  min-height: 100vh;
   overflow-x: hidden !important;
-  width: 100%;
 }
 html {
   overflow-x: hidden !important;
 }
 * {
   overflow-x: hidden !important;
+}
+.is-flex {
+  display: flex;
+}
+input {
+  padding: 9px 5px;
+  border-radius: 3px;
+  color: rgb(74, 81, 101);
+  border: none;
+  outline: none;
+  box-shadow: 1px 4px 3px black;
+}
+
+button {
+  padding: 9px;
+  border-radius: 3px;
+  border: none;
+  outline: none;
 }
 </style>
