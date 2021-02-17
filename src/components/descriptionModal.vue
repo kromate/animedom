d983acd2040ca8a1985a5ba880a37cadcd194ed6
kromate <akpananthony33@gmail.com>
2021-02-17 21:04:31 +0100

    removed bulma

--- src/components/descriptionModal.vue
+++ src/components/descriptionModal.vue
@@ -1,21 +1,23 @@
 <template>
   <transition name="slide" appear>
-    <progress class="progress is-uni " max="70" v-if="!data.id && modal">1110%</progress>
+    <div v-if="modal">
+      <progress class="progress is-uni " max="70" v-if="!data.id && modal">1110%</progress>
 
-    <div :class="[modal ? 'is-active' : '', 'modal']" v-else>
-      <div class="modal-background"></div>
-      <div class="modal-card">
-        <header class="modal-card-head">
-          <p class="modal-card-title">{{ data.name }}</p>
-          <button class="delete" aria-label="close" @click="close"></button>
-        </header>
-        <section class="modal-card-body">
-          {{ data.summary }}
-        </section>
-        <footer class="modal-card-foot">
-          <button class="button is-success">Save changes</button>
-          <button class="button" @click="close">Cancel</button>
-        </footer>
+      <div :class="[modal ? 'is-active' : '', 'modal']" v-else>
+        <div class="modal-background"></div>
+        <div class="modal-card">
+          <header class="modal-card-head">
+            <p class="modal-card-title">{{ data.name }}</p>
+            <button class="delete" aria-label="close" @click="close"></button>
+          </header>
+          <section class="modal-card-body">
+            {{ data.summary }}
+          </section>
+          <footer class="modal-card-foot">
+            <button class="button is-success">Save changes</button>
+            <button class="button" @click="close">Cancel</button>
+          </footer>
+        </div>
       </div>
     </div>
   </transition>
